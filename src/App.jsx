736ecbffca559d539f23282a95942f7a6c7ef87b
shubham sharma <shubhamtech004@gmail.com>
2025-05-29 20:10:08 +0530

    code debuging

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from "react"
 import Navbar from "./Components/Navbar"
 import Footer from "./Components/Footer"
 
-function App() {
+const  App =()=> {
   return (
     <div className="flex flex-col min-h-screen">
       <Navbar />
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faBars, faXmark } from '@fortawesome/free-solid-svg-icons';
 
-function Navbar() {
+const Navbar =()=> {
   const [isOpen, setIsOpen] = useState(false);
 
   return (
