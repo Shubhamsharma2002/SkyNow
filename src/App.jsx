e88b46683208c8f14178d46d0290c89fd4428de6
shubham sharma <shubhamtech004@gmail.com>
2025-05-29 19:40:46 +0530

    navbar components done

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
 
 import React from "react"
+import Navbar from "./Components/Navbar"
 
 function App() {
   
 
   return (
+    <>
+    <Navbar/>
     <h1 className='text-3xl text-blue-400'>weather app cooming soon</h1>
+    </>
+    
   )
 }
 
---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Navbar() {
+  return (
+    <div className='p-3 bg-amber-600 flex justify-around'>
+        <div className="">Logo</div>
+        <div className="">About</div>
+        <div className="">Contact</div>
+    </div>
+  )
+}
+
+export default Navbar
