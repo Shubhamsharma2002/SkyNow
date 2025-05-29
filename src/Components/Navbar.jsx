4da28b01441f15689b05f0c8fec2e2356f1fbca2
shubham sharma <shubhamtech004@gmail.com>
2025-05-29 20:00:32 +0530

    responsive navbar done

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,13 +1,39 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBars, faXmark } from '@fortawesome/free-solid-svg-icons';
 
 function Navbar() {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
-    <div className='p-3 bg-amber-600 flex justify-around'>
-        <div className="">Logo</div>
-        <div className="">About</div>
-        <div className="">Contact</div>
-    </div>
-  )
+    <nav className="bg-amber-600 p-3">
+      <div className="flex justify-around items-center">
+        <div className="text-white font-bold text-lg">Logo</div>
+
+        {/* Mobile menu button */}
+        <button
+          className="text-white md:hidden"
+          onClick={() => setIsOpen(!isOpen)}
+        >
+          <FontAwesomeIcon icon={isOpen ? faXmark : faBars} size="lg" />
+        </button>
+
+        {/* Desktop menu */}
+        <div className="hidden md:flex space-x-6 text-white">
+          <a href="#">About</a>
+          <a href="#">Contact</a>
+        </div>
+      </div>
+
+      {/* Mobile dropdown */}
+      {isOpen && (
+        <div className="flex flex-col mt-2 text-center space-y-2 text-white md:hidden">
+          <a href="#">About</a>
+          <a href="#">Contact</a>
+        </div>
+      )}
+    </nav>
+  );
 }
 
-export default Navbar
+export default Navbar;
