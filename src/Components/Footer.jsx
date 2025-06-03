43e7953dffa3507e0071060fdea565a79511471d
shubham sharma <shubhamtech004@gmail.com>
2025-06-03 18:18:05 +0530

    adding font awesome , weather componenet

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Footer = () => {
   return (
-    <footer className="bg-gray-900 text-white py-6 mt-10">
+    <footer className="bg-gray-900 text-white py-6 ">
       <div className="max-w-7xl mx-auto px-4 flex flex-col md:flex-row items-center justify-around">
         <p className="text-sm text-gray-400">
           &copy; {new Date().getFullYear()} Shubham Sharma. All rights reserved.
---
+++ src/Components/Weather.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons'
+
+const Weather = () => {
+  return (
+    <div className="flex justify-center bg-blue-100 pt-20 min-h-screen">
+      
+    </div>
+  )
+}
+
+export default Weather
